26cf5879740482d29bd28d2c9db4d8664b263a17
XenGraf <79160125+XenGraf@users.noreply.github.com>
2021-05-22 12:04:46 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_page_contain_add_to_basket_btn(browser):
+    browser = webdriver.Chrome()
     browser.get(link)
     time.sleep(30)
     basket_btn = browser.find_element_by_css_selector("button.btn-add-to-basket")
